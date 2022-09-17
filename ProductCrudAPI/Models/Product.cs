1612f04b1aa4665ecdda91fb3f9d3c601abebdfb
wendelsimoes <wendel.simoes.br@gmail.com>
2022-09-17 19:14:40 -0300

    Add project files.

---
+++ ProductCrudAPI/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductCrudAPI.Models
+{
+    public partial class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
